0ba6de5b9c5422a58f77acd89e0baeb9bd489ba7
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-01-29 16:48:20 -0600

    got the dumb paging to work

--- js/client.js
+++ js/client.js
@@ -1,31 +1,50 @@
 $(function () {
-  getEvents(1)
+  getEvents(1);
+  var currentDisplayPage = 1;
+
+  // buttons
+  $("#nexPage").click(function () {
+    currentDisplayPage ++;
+    console.log("page: " + currentDisplayPage);
+    getEvents(currentDisplayPage);
+
+  });
+
 
   function getEvents(page) {
     $.getJSON({
       url: "https://modasclient.azurewebsites.net/api/event/pagesize/10/page/" + page,
       success: function (response, textStatus, jqXhr) {
-        console.log(response);
+        // console.log(response);
         //use response to get the data
-        // response..page
-        // response.pagingInfo.currentPage
-        // response.events.
+        let eventTable = document.getElementById("tableContent");
+        // console.log(eventTable.rows.length);
+        while (eventTable.rows.length > 0) {
+          eventTable.deleteRow(0);
+          // console.log("this is the for loop: " + eventTable.rows.length);
+        }
+
+        // console.log(eventTable.rows.length);
 
         response.events.forEach(element => {
-          console.log(element);
-          
-          $('#table')
-            .append(
-              '<tr>' + 
-              '<td id="flag">' + element.flag + '</td>' + 
-              '<td id="flag">' + element.stamp + '</td>' + 
-              '<td id="flag">' + element.id + '</td>' + 
-              '<td id="flag">' + element.loc + '</td>' + 
-              '<tr>'
-              )
-            .addClass('myclass');
-        });
+          console.log("this is after the data is added " + eventTable.rows.length);
+          // console.log(element);
 
+          $('#tableContent')
+            .append(
+              '<tr>' +
+              '<td>' + element.flag + '</td>' +
+              '<td>' + element.stamp + '</td>' +
+              '<td>' + element.id + '</td>' +
+              '<td>' + element.loc + '</td>' +
+              '<tr>'
+            )
+            console.log(element.id)
+            
+        });
+        // $('#displayPageInfo')
+        //   .append(response.pagingInfo.rangeStart + "-" + response.pagingInfo.rangeEnd + " of " + response.pagingInfo.totalItems + " events")
+          
       },
       error: function (jqXHR, textStatus, errorThrown) {
         // log the error to the console
@@ -33,4 +52,5 @@ $(function () {
       }
     });
   }
+
 });
