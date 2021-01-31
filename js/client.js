999e87583e42c84cc309014b6c460df97124ff7d
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-01-30 20:27:28 -0600

    added the buttons for paging and now display the page info the right was as well

--- js/client.js
+++ js/client.js
@@ -3,10 +3,57 @@ $(function () {
   var currentDisplayPage = 1;
 
   // buttons
-  $("#nexPage").click(function () {
-    currentDisplayPage ++;
-    console.log("page: " + currentDisplayPage);
+  $("#firPage").click(function () {
+    console.log("first page");
+    currentDisplayPage = 1;
     getEvents(currentDisplayPage);
+
+  });
+
+  $("#prePage").click(function () {
+
+    if (currentDisplayPage > 1) {
+      console.log(currentDisplayPage);
+      currentDisplayPage--;
+      getEvents(currentDisplayPage);
+    }
+
+
+  });
+
+  $("#nexPage").click(function () {
+    $.getJSON({
+      url: "https://modasclient.azurewebsites.net/api/event/pagesize/10/page/" + currentDisplayPage,
+      success: function (response, textStatus, jqXhr) {
+
+        if (currentDisplayPage < response.pagingInfo.totalPages) {
+          currentDisplayPage++;
+          getEvents(currentDisplayPage);
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        // log the error to the console
+        console.log("The following error occured: " + textStatus, errorThrown);
+      }
+    });
+
+  });
+
+  $("#lasPage").click(function () {
+    $.getJSON({
+      url: "https://modasclient.azurewebsites.net/api/event/pagesize/10/page/" + currentDisplayPage,
+      success: function (response, textStatus, jqXhr) {
+        console.log(" last page button: " + response);
+
+        currentDisplayPage = response.pagingInfo.totalPages;
+        console.log("page: " + currentDisplayPage);
+        getEvents(currentDisplayPage);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        // log the error to the console
+        console.log("The following error occured: " + textStatus, errorThrown);
+      }
+    });
 
   });
 
@@ -15,7 +62,7 @@ $(function () {
     $.getJSON({
       url: "https://modasclient.azurewebsites.net/api/event/pagesize/10/page/" + page,
       success: function (response, textStatus, jqXhr) {
-        // console.log(response);
+        console.log(response);
         //use response to get the data
         let eventTable = document.getElementById("tableContent");
         // console.log(eventTable.rows.length);
@@ -24,10 +71,11 @@ $(function () {
           // console.log("this is the for loop: " + eventTable.rows.length);
         }
 
+
         // console.log(eventTable.rows.length);
 
         response.events.forEach(element => {
-          console.log("this is after the data is added " + eventTable.rows.length);
+          // console.log("this is after the data is added " + eventTable.rows.length);
           // console.log(element);
 
           $('#tableContent')
@@ -39,12 +87,27 @@ $(function () {
               '<td>' + element.loc + '</td>' +
               '<tr>'
             )
-            console.log(element.id)
-            
+          // console.log(element.id)
+
         });
+
+        let newPageInfo = response.pagingInfo.rangeStart + "-" + response.pagingInfo.rangeEnd + " of " + response.pagingInfo.totalItems + " events";
+        console.log(newPageInfo);
         // $('#displayPageInfo')
-        //   .append(response.pagingInfo.rangeStart + "-" + response.pagingInfo.rangeEnd + " of " + response.pagingInfo.totalItems + " events")
-          
+        // .append(response.pagingInfo.rangeStart + "-" + response.pagingInfo.rangeEnd + " of " + response.pagingInfo.totalItems + " events")
+        
+        $('#displayPageInfo').text(newPageInfo);
+        // let tableInfo = document.getElementById("displayPageInfo");
+        // if ($('#displayPageInfo').is(':empty') == false) {
+        //   console.log("table button" + tableInfo);
+        //   tableInfo.remove();
+        // }
+
+
+
+
+
+
       },
       error: function (jqXHR, textStatus, errorThrown) {
         // log the error to the console
